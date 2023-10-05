e66cd5bc92a2201ac36be46be74f6a7d5178dbdb
essex <a.sh4dow@yahoo.com>
2023-10-05 14:38:04 +0300

    123
    123

--- src/app/foodtracking/page.tsx
+++ src/app/foodtracking/page.tsx
@@ -1,13 +1,101 @@
+"use client"
+// pages/food-logger.js
+import { PrismaClient } from '@prisma/client';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+//
 
 
-export default function Page() {
+export default function FoodLogger() {
+  const router = useRouter();
+  const [foodName, setFoodName] = useState('');
+  const [calories, setCalories] = useState('');
+  const [carbohydrates, setCarbohydrates] = useState('');
+  const [protein, setProtein] = useState('');
+
+  const handleAddFood = async () => {
+    if (foodName && calories && carbohydrates && protein) {
+      try {
+        const newEntry = await prisma.foodEntry.create({
+          data: {
+            foodName,
+            calories: parseInt(calories),
+            carbohydrates: parseInt(carbohydrates),
+            protein: parseInt(protein),
+          },
+        });
+
+        setFoodName('');
+        setCalories('');
+        setCarbohydrates('');
+        setProtein('');
+      } catch (error) {
+        console.error('Error adding food entry:', error);
+      }
+    } else {
+      console.error('Please fill in all fields');
+    }
+  };
+
+  const calculateTotalCalories = async () => {
+    try {
+      const foodEntries = await prisma.foodEntry.findMany();
+      const totalCalories = foodEntries.reduce(
+        (total, entry) => total + entry.calories,
+        0
+      );
+      return totalCalories;
+    } catch (error) {
+      console.error('Error calculating total calories:', error);
+      return 0;
+    }
+  };
+
   return (
     <div>
-
-      <main style={{ display: 'flex', minHeight: '100vh', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
-
-        <p style={{ fontSize: '1.2rem' }}>Still working on it!</p>
-      </main>
+      <h1>Food Logger</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Food Name"
+          value={foodName}
+          onChange={(e) => setFoodName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Calories"
+          value={calories}
+          onChange={(e) => setCalories(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Carbohydrates (g)"
+          value={carbohydrates}
+          onChange={(e) => setCarbohydrates(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Protein (g)"
+          value={protein}
+          onChange={(e) => setProtein(e.target.value)}
+        />
+        <button onClick={handleAddFood}>Add Food</button>
+      </div>
+      <div>
+        <h2>Food Entries</h2>
+        <ul>
+          {foodEntries.map((entry, index) => (
+            <li key={index}>
+              {entry.foodName} - {entry.calories} calories
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Total Calories Today</h2>
+        <p>{calculateTotalCalories()} calories</p>
+      </div>
     </div>
   );
 }
