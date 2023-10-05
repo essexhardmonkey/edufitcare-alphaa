5cdc4fd7ac152a6f7ae4d003e0d19a1eb1f5d81f
essex <a.sh4dow@yahoo.com>
2023-10-05 13:01:35 +0300

    update

---
+++ src/app/foodtracking/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Page() {
+  return (
+    <div>
+
+      <main style={{ display: 'flex', minHeight: '100vh', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+
+        <p style={{ fontSize: '1.2rem' }}>Still working on it!</p>
+      </main>
+    </div>
+  );
+}
